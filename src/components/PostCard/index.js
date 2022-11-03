755e221abfb05bb89740422875cc895ff064aadf
Cristina Jung <cristijung@gmail.com>
2022-11-02 23:47:05 -0300

    tela inicio pronta

---
+++ src/components/PostCard/index.js
@@ -0,0 +1,22 @@
+import styles from './Post.module.css';
+
+//odas as imagens deverão ter o mesmo nome e extensão -- linha 11
+//{/** Btn provisório */} 
+             
+export default function PostCard({ post }) {
+    return(
+        <>
+        <div className={styles.post}>
+            <img 
+                className={styles.capa}
+                src={`/assets/posts/${post.id}/img.jpg`}  
+                alt="Imagem do Post"
+            />
+            <h2 className={styles.titulo}>{post.titulo}</h2>
+            <button className={styles.botaoLer}>Ler</button>        
+            
+        </div>
+
+        </>
+    )
+}
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,11 +1,20 @@
 import Banner from "components/Banner"; //configuração de importação absoluta do React
+import styles from './Inicio.module.css';
+import posts from 'json/posts.json';
+import PostCard from "components/PostCard";
 
 function Inicio () {
     return(
         <>
         <main>
-            <Banner />        
-           <h1> Este é o início </h1>
+            <Banner />  
+            <ul className={styles.posts}>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <PostCard post={post} />
+                    </li>
+                ))}                
+            </ul>           
         </main>
         </>
     );
